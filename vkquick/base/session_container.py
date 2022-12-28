827752ab4642e76131e54533a4bb922eb1e9cc72
IINaGiBaToRII <71980719+IINaGiBaToRII@users.noreply.github.com>
2022-12-28 19:31:33 +0200

    Update session_container.py

--- vkquick/base/session_container.py
+++ vkquick/base/session_container.py
@@ -98,7 +98,7 @@ class SessionContainerMixin:
         """
         return aiohttp.ClientSession(
             connector=aiohttp.TCPConnector(ssl=ssl.SSLContext()),
-            headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"},
+            skip_auto_headers={"User-Agent"},
             raise_for_status=True,
             json_serialize=self.__json_parser.dumps,
         )
